7cf4822050d5ad24534d89950a114f33e020f331
Ganna Ovchynnykova <ov4innikova.an@gmail.com>
2021-02-19 00:51:03 +0100

    Cats and Users

--- 2021_02_18_OOP_cats_Encapsulation/src/de/andron13/data/User.java
+++ 2021_02_18_OOP_cats_Encapsulation/src/de/andron13/data/User.java
@@ -28,13 +28,48 @@ public class User {
         this.country = country;
         this.password = password;
         this.mobilNumber = mobilNumber;
+
         setBodyMassIndex();
         setGeneratorPassword();
 
     }
+    void setEmail (String newEmail){
+        this.email = newEmail;
+    }
+    public String getEmail (){
+        return email;
+    }
+    void setMobilNumber(String newNomberMob){
+        this.mobilNumber = newNomberMob;
+    }
+    public String getMobilNumber(){
+        return mobilNumber;
+    }
+    void setCountry (String newCountry){
+        this.country = newCountry;
+    }
+    public String getCountry (){
+        return country;
+    }
+    void setDateOfBirth (String newDate){
+        this.dateOfBirth = newDate;
+    }
+    public String getDate (){
+        return dateOfBirth;
+    }
+    void setName (String newName){
+        this.name = newName;
+    }
+    public String getName (){
+        return name;
+    }
     private int setGeneratorPassword(){
         for (int i = (int) (Math.random()*10); i < 9; i++) {
+            password = i;
             }
+        return password;
+    }
+    public int getRandomPassword(){
         return password;
     }
 
@@ -46,9 +81,6 @@ public class User {
     public double getBodyMassIndex(){
         return bodyMassIndex;
     }
-    public int getRandomPassword(){
-        return password;
-    }
 }
 
 class TestUser{
@@ -57,5 +89,16 @@ class TestUser{
                 "02.08.1990", "Ukraine", 5555555, "23-87-45");
         System.out.println(myLovelyUser.getBodyMassIndex());
         System.out.println(myLovelyUser.getRandomPassword());
+        myLovelyUser.setName("Martin");
+        System.out.println(myLovelyUser.getName());
+        myLovelyUser.setDateOfBirth("03. 04, 1995");
+        System.out.println(myLovelyUser.getDate());
+        myLovelyUser.setCountry("Kanada");
+        System.out.println(myLovelyUser.getCountry());
+        myLovelyUser.setMobilNumber("54858");
+        System.out.println(myLovelyUser.getMobilNumber());
+        myLovelyUser.setEmail("gfj@com");
+        System.out.println(myLovelyUser.getEmail());
+
     }
 }
